b8be92a28ac88251f654ce39586ec40757c58a53
Vidushi Garg <vidushigarg@Vidushis-MacBook-Pro.local>
2020-06-28 15:24:12 -0400

    Adding files

---
+++ build/precache-manifest.7e95586f33d453e11f9b17728f5bea25.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b947973849101a2e818",
+    "url": "/me/static/css/main.07318036.chunk.css"
+  },
+  {
+    "revision": "0b947973849101a2e818",
+    "url": "/me/static/js/main.0b947973.chunk.js"
+  },
+  {
+    "revision": "66be97a19e235fa95a6c",
+    "url": "/me/static/js/runtime~main.66be97a1.js"
+  },
+  {
+    "revision": "8f6cfff7af5b749328f5",
+    "url": "/me/static/js/2.8f6cfff7.chunk.js"
+  },
+  {
+    "revision": "a94c18b6e52ad2ddcb7a7b76ac8129a9",
+    "url": "/me/index.html"
+  }
+];
