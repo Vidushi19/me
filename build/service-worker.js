37a364e56e85deb0781c5b852dfc5cdee4aeab02
Vidushi Garg <vidushigarg@Vidushis-MacBook-Pro.local>
2020-06-29 08:58:00 -0400

    Pruned a bit

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/me_/precache-manifest.9dfc78c63b2ac3b7a24dd0a1618586c6.js"
+  "/me_/precache-manifest.85385c85b841847ea6f0caec214dc175.js"
 );
 
 workbox.clientsClaim();
